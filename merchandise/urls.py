8fb9b9a56bae375ae2d6db7bf7cefe3f233c173a
brekih <brekih@gmail.com>
2021-05-06 15:02:25 +0000

    Renamed manufacturer to merchandise and made the sidebar and centered the search bar

--- merchandise/urls.py
+++ merchandise/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # http://localhost:8000/manufacturers
+    # http://localhost:8000/merchandise
     path('', views.index, name="index"),
 ]
--- ship_o_cereal/urls.py
+++ ship_o_cereal/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cereals/', include('cereal.urls')),
-    path('manufacturers/', include('manufacturer.urls'))
+    path('merchandise/', include('merchandise.urls'))
 ]
